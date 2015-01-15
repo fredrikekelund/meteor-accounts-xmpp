a972a2b7a997f31d45967cb77cadb1f1157232f0
Fredrik Ekelund <bohemenian@gmail.com>
2015-01-15 11:50:21 +0100

    Fixed package.js

--- package.js
+++ package.js
@@ -6,13 +6,12 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-	api.use("froatsnook:sleep", "server");
+	api.use("froatsnook:sleep@=1.0.0", "server");
 	api.use("fredrik:node-xmpp@=1.0.0", "server");
 	api.use("accounts-base@=1.1.3", ["client", "server"]);
-	api.use("check", "server");
+	api.use("check@=1.0.3", "server");
 	api.imply("accounts-base", ["client", "server"]); // Export Accounts (etc) to packages using this one.
 
-	api.addFiles("accounts-xmpp.config.js", ["client", "server"]);
 	api.addFiles("accounts-xmpp.client.js", "client");
 	api.addFiles("accounts-xmpp.server.js", "server");
 });
@@ -20,5 +19,6 @@ Package.onUse(function (api) {
 Package.onTest(function (api) {
 	api.use("tinytest");
 	api.use("fredrik:accounts-xmpp");
+	api.addFiles("accounts-xmpp.config.js", ["client", "server"]);
 	api.addFiles("accounts-xmpp.test.js", "client");
 });
